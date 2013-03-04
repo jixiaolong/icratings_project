3063e9c09bc0fb30192a267daaaba4b2302ec602
Darren <darren.ma@supremosolutions.com>
2013-03-05 00:32:22 +0200

    cleaned up urls

--- icratings_project/icratings_project/urls.py
+++ icratings_project/icratings_project/urls.py
@@ -5,18 +5,8 @@ from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
-from django.conf import settings
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='base.html')),
-
-    # Examples:
-    # url(r'^$', 'icratings_project.views.home', name='home'),
-    # url(r'^icratings_project/', include('icratings_project.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
