6b9c3a8a9f570f825fda6fc713a09ee341d7cd4f
Darren <darren.ma@supremosolutions.com>
2013-03-04 21:23:43 +0200

    serving static via django even with debug false

--- icratings_project/icratings_project/urls.py
+++ icratings_project/icratings_project/urls.py
@@ -5,6 +5,8 @@ from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
+from django.conf import settings
+
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='base.html')),
 
@@ -17,4 +19,10 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('',
+    (r'^static/(.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT
+    }),
 )
